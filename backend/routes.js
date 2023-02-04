081c624191d44bfca58860b93821dc86d21fc526
Adam von Arnim <vonarnim@usc.edu>
2023-02-04 11:27:14 -0800

    api connection established

--- backend/routes.js
+++ backend/routes.js
@@ -1,10 +1,11 @@
 "use strict";
 const router = require("express").Router();
-const spot = require("./controllers/spotController");
+const spot = require("./controllers/user");
 
 // trip-user Routes
 router.route("/api").get((req, res) => {
   console.log("hello world");
+  res.status(200).json({ message: "Hello world!" });
 });
 
 module.exports = router;
